63e1e306fa3e9219fa83854853bf79a86847dcb8
Trong <20520330@gm.uit.edu.vn>
2025-04-14 23:56:03 +0700

    feat: integrate qdrant db

--- memePipeline.py
+++ memePipeline.py
@@ -1,43 +1,43 @@
-from faissDB import FAISS
 
+from qdrant_db_connect import QdrantDBConnection
 from handleMeme import MemeGenerator 
 import numpy as np
 import pandas as pd
-
+from utils import get_embedding, get_description_for_image
 
 
 class MemePipeline:
-    def __init__(self, faiss_index_path, meme_generator):
-        self.faiss = FAISS()
-        self.faiss.load_index(faiss_index_path)
+    def __init__(self,qdrant_url,vector_emdding_path, meme_generator):
+        self.qdrant_client = QdrantDBConnection(url=qdrant_url)
+        self.embedding = np.load(vector_emdding_path)
         self.meme_generator = meme_generator
-        self.df = pd.read_csv("meme_data.csv")
+        self.df = get_description_for_image('./meme_data.csv', num_rows=10, get_all=False)
+        self.qdrant_client.index_data(self.df, self.embedding)
 
     def query_image_and_caption(self, query, k=2):
-        query_embedding = np.array([self.meme_generator.get_embedding(query)]).astype("float32")
-        distances, indices = self.faiss.search(query_embedding, k)
+     
+        results = self.qdrant_client.search(query, limit=k)
         list_of_memes = []
         list_image_path = []
-        for i in range(k):
-            meme = self.faiss.texts[indices[0][i]]
-            list_of_memes.append(meme)
-            list_image_path.append(self.df['image_path'][indices[0][i]])
-        
+        for result in results:
+            meme_context = result.payload['text']
+            image_path = result.payload['name']
+            list_of_memes.append(meme_context)
+            list_image_path.append(image_path)
         return list_of_memes, list_image_path
 
     def pipeline(self, query, k=2):
         list_meme = []
-        for user_context, image_path in zip(*self.query_image_and_caption(query, k)):
-            meme = self.meme_generator.generate_meme(user_context, image_path)
+        for meme_context, image_path in zip(*self.query_image_and_caption(query, k)):
+            meme = self.meme_generator.generate_meme(meme_context, image_path)
             list_meme.append(meme)
             
         return list_meme
 
-    
 if __name__ == "__main__":
     
     user_input = "Meme Title: And Just Like That"
-    faiss_index_path = "vectordb.index"
+    vector_emdding_path = "embeddings.npy"
     meme_generator = MemeGenerator()
-    meme_pipeline = MemePipeline(faiss_index_path, meme_generator)
-    results = meme_pipeline.search_and_generate(user_input, k=2)
+    meme_pipeline = MemePipeline("http://103.186.100.39:6333", vector_emdding_path, meme_generator)
+    results = meme_pipeline.pipeline(user_input, k=2)
